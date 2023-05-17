f37ac303ed700e41c7e7d750b7df8cd2e304cbf7
Allah-Rakha <120125497+AllahRakha1234@users.noreply.github.com>
2023-05-18 02:11:50 +0500

    redirect.js

--- redirect.js
+++ redirect.js
@@ -1,58 +1,44 @@
-// Get the current URL
+// GET THE URL OF THE CURRENT PAGE
 var currentUrl = window.location.href;
 
-// Set the value of the hidden input field to the current URL
+// SET THE VALUE OF THE HIDDEN INPUT FIELD TO THE CURRENT URL
 document.getElementById("urlField").value = currentUrl;
-// Show an alert message when the response is received
+// FUNCTION TO SHOW ALERT
 function showAlert(message) {
   alert(message);
 }
-function changepopup(data) {
-  console.log(data);
-  if (data.disease === "Protonopia") {
-    console.log("Protonopia");
-    var popup = document.querySelector(".protonopia");
-    var info = document.querySelector(".info");
-    info.style.display = "none";
-    popup.style.display = "block";
-  }
+// FORMDATA (TO GET THE DATA FROM THE FORM) (EVENT.TARGET -> FORM)
+var formData = new FormData(event.target);
+const data = {};
+for (let pair of formData.entries()) {
+  data[pair[0]] = pair[1]; // CREATING KEY-VALUE PAIR OF ALL FIELDS OF DATA
 }
-// Submit the form using AJAX
+xhr.send(JSON.stringify(data)); // SENDING THE DATA TO THE SERVER
+// GETTING THE DATA FROM THE FORM AND SENDING IT TO THE SERVER AND THEN RECEIVING THE RESPONSE
 document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://chroma.ap-1.evennode.com/");
-  xhr.setRequestHeader("Content-Type", "application/json");
+  // XMLHttpRequest (TO SEND AND RECEIVE DATA TO SERVER DYNAMICALLY AND ASYNCHRONOUSLY)
+  var xhr = new XMLHttpRequest(); // NEW XMLHTTPREQUEST OBJECT
+  xhr.open("POST", "http://chroma.ap-1.evennode.com/"); // OPENING A CONNECTION (URL OF THE SERVER TO SEND DATA)
+  xhr.setRequestHeader("Content-Type", "application/json"); // SETTING THE HEADER OF THE REQUEST
+  // FUNCTION (CALLS WHEN THE STATE OF REQUEST CHANGES)
   xhr.onreadystatechange = function () {
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      // 200 -> SUCCESSFUL RESPONSE | .DONE -> PROPERTY OF XHR -> REQUEST HAS COMPLETED
       var data = JSON.parse(this.responseText);
       showAlert(data.msg);
+      // SAVING THE DATA IN LOCAL STORAGE TO USE IT FURTHER
       localStorage.setItem("mydata", JSON.stringify(data));
-
+      // TO SEND DATA TO BACKGROUND SCRIPT (background.js)
       chrome.runtime.sendMessage(
-        { message: "sendData", data: data },
-        function (response) {
+        { message: "sendData", data: data }, // MESSAGE TO BE SENT
+        function (response) {  // CALLBACK FUNCTION
           console.log("Response from background script:", response);
         }
       );
-      // localStorage.setItem("disease", data.disease);
-      // localStorage.setItem("name", data.name);
-      ///////////////////////////////////////////////////////////
-      // var script2 = document.createElement("script");
-      // // Set the source of the script to script2.js
-      // script2.src = "test.js";
-      // // Append the script element to the document's body
-      // document.body.appendChild(script2);
-      ////////////////////////////////////////////////////
+      // IF PAGE IS NOT FOUND 
     } else if (this.readyState === XMLHttpRequest.DONE && this.status === 404) {
       showAlert(data.msg);
     }
   };
-  var url = new FormData(event.target);
-  const data = {};
-  for (let pair of url.entries()) {
-    data[pair[0]] = pair[1]; // Create a key-value pair for each form field
-  }
-  // console.log(data);
-  xhr.send(JSON.stringify(data));
-});
+}); 
